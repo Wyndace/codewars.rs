0ba889b03ec22422fe0731fd5ee98bda2813967e
wyndace <wyndace.av@ya.ru>
2024-11-09 14:14:56 +0300

    solve: function 1 – Hello World!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod tasks;
 
 fn main() {
-    println!("{}", tasks::traffic_light::update_light("yellow"));
-    println!("{}", tasks::traffic_light::update_light("red"));
-    println!("{}", tasks::traffic_light::update_light("green"));
+    println!("{}", tasks::hello_world::greet());
 }
