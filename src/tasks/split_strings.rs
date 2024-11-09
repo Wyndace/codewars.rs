d412dbb3d87eb1d4da0c4c52fc3974481ba7bd35
wyndace <wyndace.av@ya.ru>
2024-11-09 20:37:18 +0300

    refactor: Split Strings

--- src/tasks/split_strings.rs
+++ src/tasks/split_strings.rs
@@ -1,16 +1,12 @@
 pub fn solution(s: &str) -> Vec<String> {
-    let mut result = vec![];
     let mut s = s.to_owned();
-    let mut result_str = String::new();
     if s.len() % 2 != 0 {
         s.push('_');
     }
-    for c in s.chars() {
-        result_str.push(c);
-        if result_str.len() == 2 {
-            result.push(result_str.clone());
-            result_str = String::new()
-        }
-    }
-    result
+    s
+        .chars()
+        .collect::<Vec<_>>()
+        .chunks(2)
+        .map(|chunk| chunk.iter().collect::<String>())
+        .collect::<Vec<String>>()
 }
