52700592c12b1a1e112754c2085ba854ee94f159
wyndace <wyndace.av@ya.ru>
2024-11-09 14:05:39 +0300

    solve: Counting sheep...

---
+++ src/tasks/counting_sheep.rs
@@ -0,0 +1,3 @@
+pub fn count_sheep(sheep: &[bool]) -> u8 {
+    sheep.iter().filter(|x| **x).count() as u8
+}
--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod traffic_light;
 pub(crate) mod rock_paper_scissors;
+pub(crate) mod counting_sheep;
